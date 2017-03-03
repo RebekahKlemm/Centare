de9124c7f3255a0d0a12256dcdeee2aaa9322ffe
Rebekah <Rebekah@REBEKAHDELLDESK>
2017-03-03 08:39:48 -0600

    Add project files.

---
+++ CheckInWeb/Scripts/MyCheckInViewModel.js
@@ -0,0 +1,10 @@
+﻿function MyCheckInViewModel(model) {
+    var self = this;
+
+    self.pageSize = ko.observable(10);
+    self.checkIns = ko.observableArray(model.CheckIns);
+
+    self.checkInPage = ko.computed(function () {
+        return self.checkIns().slice(0, self.pageSize());
+    });
+}
